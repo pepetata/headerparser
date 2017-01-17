16a9fa9ee8bf9a605d4dd3c8437f3bf44260de5c
Flavio Ferreira <flavio_luiz_ferreira@hotmail.com>
2017-01-17 13:11:56 +0000

    first version

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express')
  , app = express()
+ // per http://stackoverflow.com/questions/11845471/how-can-i-get-browser-the-language-in-node-js-express-js
+ , accepts = require('accepts')
+ 
 // per http://stackoverflow.com/questions/10849687/express-js-how-to-get-remote-client-address
 app.enable('trust proxy')
 
 app.get('*', function (req, res) {
-    console.log(req.ips,req.ip, req.headers["accept-language"] ,req.headers['user-agent'],req)
+    var lang = accepts(req).languages()[0]
+    var os = req.headers['user-agent'].toString()
+    os = os.substr(os.indexOf('(')+1,os.indexOf(')') - os.indexOf('(') -1)
+    var resp = {ip: req.ip, language: lang, operating_system: os }
+    console.log(resp)
+    res.end(JSON.stringify(resp))
 })
 
 app.listen(8080, function () {
